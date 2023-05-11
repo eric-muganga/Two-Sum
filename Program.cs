a5fe27f404632edcbb58c26eab15b7928c66eadc
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2023-05-11 02:00:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//time complexity of O(n^2) and space complexity is O(1)
+
+int[] nums = { 11, 7, 2, 15 };
+int target = 9;
+
+int[] result = TwoSum(nums, target);
+Console.WriteLine(string.Join(", ", result));
+
+int[] TwoSum(int[] nums, int target)
+{
+
+    int[] indices = new int[2];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                indices[0] = i;
+                indices[1] = j;
+            }
+        }
+
+    }
+    return indices;
+}
